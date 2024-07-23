61e760b053909c26ac2d7a884f303f6923ef1437
Spark <legion20220808@gmail.com>
2024-07-23 15:13:08 +0800

    modified:   .gitignore 	modified:   Figure_2.png 	new file:   JSCCNet.pth 	modified:   main.py

--- main.py
+++ main.py
@@ -39,15 +39,15 @@ class JSCCNet(nn.Module):
     def __init__(self, P, snr_db, input_channels=3, output_channels=3, wireless_channel=19):
         super(JSCCNet, self).__init__()
         # Encoder
-        self.conv1 = nn.Conv2d(input_channels, 16, kernel_size=5, stride=2, padding=0)
+        self.conv1 = nn.Conv2d(input_channels, 16, kernel_size=4, stride=2, padding=2)
         self.prelu1 = nn.PReLU()
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2, padding=0)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=4, stride=2, padding=0)
         self.prelu2 = nn.PReLU()
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=2)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=1, padding=2)
         self.prelu3 = nn.PReLU()
-        self.conv4 = nn.Conv2d(32, 19, kernel_size=3, stride=1, padding=2)
+        self.conv4 = nn.Conv2d(32, 19, kernel_size=5, stride=1, padding=2)
         self.prelu4 = nn.PReLU()
-        self.conv5 = nn.Conv2d(19, wireless_channel, kernel_size=3, stride=1, padding=2)
+        self.conv5 = nn.Conv2d(19, wireless_channel, kernel_size=5, stride=1, padding=2)
         self.prelu5 = nn.PReLU()
 
         # Normalization and AWGN
@@ -55,15 +55,15 @@ class JSCCNet(nn.Module):
         self.awgn = AWGNLayer(snr_db)
 
         # Decoder
-        self.deconv1 = nn.ConvTranspose2d(wireless_channel, 19, kernel_size=3, stride=1, padding=2)
+        self.deconv1 = nn.ConvTranspose2d(wireless_channel, 19, kernel_size=5, stride=1, padding=2)
         self.prelu6 = nn.PReLU()
-        self.deconv2 = nn.ConvTranspose2d(19, 32, kernel_size=3, stride=1, padding=2)
+        self.deconv2 = nn.ConvTranspose2d(19, 32, kernel_size=5, stride=1, padding=2)
         self.prelu7 = nn.PReLU()
-        self.deconv3 = nn.ConvTranspose2d(32, 32, kernel_size=3, stride=1, padding=2)
+        self.deconv3 = nn.ConvTranspose2d(32, 32, kernel_size=5, stride=1, padding=2)
         self.prelu8 = nn.PReLU()
-        self.deconv4 = nn.ConvTranspose2d(32, 16, kernel_size=5, stride=2, padding=0, output_padding=1)
+        self.deconv4 = nn.ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=0, output_padding=0)
         self.prelu9 = nn.PReLU()
-        self.deconv5 = nn.ConvTranspose2d(16, output_channels, kernel_size=5, stride=2, padding=0, output_padding=1)
+        self.deconv5 = nn.ConvTranspose2d(16, output_channels, kernel_size=4, stride=2, padding=1, output_padding=0)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
@@ -88,10 +88,10 @@ class JSCCNet(nn.Module):
 
 # Hyperparameters
 batch_size = 64
-learning_rate = 1e-3
-num_epochs = 20
+learning_rate = 1e-2
+num_epochs = 100
 P = 1.0
-snr_db = 30 
+snr_db = 20 
 wireless_channel = 19
 
 # Instantiate the model and move it to the GPU if available
@@ -102,11 +102,20 @@ transform = transforms.Compose([
     transforms.ToTensor(),
 ])
 
-train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+# Choose the dataset
+if True:
+    train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-test_dataset = datasets.CIFAR10(root='./data', train=False, transform=transform, download=True)
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+    test_dataset = datasets.CIFAR10(root='./data', train=False, transform=transform, download=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+else:
+    train_dataset = datasets.STL10(root='./data', split='train', download=True, transform=transform)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+    test_dataset = datasets.STL10(root='./data', split='test', transform=transform, download=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
 # Example training loop
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
